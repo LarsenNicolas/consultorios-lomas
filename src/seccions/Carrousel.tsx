8517dbfc903f3ed683a652b905eedea59f5a1587
Lucia <luciadaniela22@gmail.com>
2023-03-12 12:08:13 -0300

    Fix path for netifly

--- src/seccions/Carrousel.tsx
+++ src/seccions/Carrousel.tsx
@@ -3,11 +3,11 @@ import { ImagenCarrousel } from "../components/ImagenCarrousel"
 export function Carrousel () {
   return (
     <section className="flex w-37 h-27 overflow-hidden px-5">
-        <ImagenCarrousel path="public/fotosConsutorio/Frente.jpg"></ImagenCarrousel>
-        <ImagenCarrousel path="public/fotosConsutorio/consulf.jpg"></ImagenCarrousel>
-        <ImagenCarrousel path="public/fotosConsutorio/consulf2.jpg"></ImagenCarrousel>
-        <ImagenCarrousel path="public/fotosConsutorio/consul2.jpg"></ImagenCarrousel>
-        <ImagenCarrousel path="public/fotosConsutorio/consul3.jpg"></ImagenCarrousel>
+        <ImagenCarrousel path="https://raw.githubusercontent.com/LarsenNicolas/consultorios-lomas/master/public/fotosConsutorio/Frente.jpg"></ImagenCarrousel>
+        <ImagenCarrousel path="https://raw.githubusercontent.com/LarsenNicolas/consultorios-lomas/master/public/fotosConsutorio/consulf.jpg"></ImagenCarrousel>
+        <ImagenCarrousel path="https://raw.githubusercontent.com/LarsenNicolas/consultorios-lomas/master/public/fotosConsutorio/consulf2.jpg"></ImagenCarrousel>
+        <ImagenCarrousel path="https://raw.githubusercontent.com/LarsenNicolas/consultorios-lomas/master/public/fotosConsutorio/consul2.jpg"></ImagenCarrousel>
+        <ImagenCarrousel path="https://raw.githubusercontent.com/LarsenNicolas/consultorios-lomas/master/public/fotosConsutorio/consul3.jpg"></ImagenCarrousel>
     </section>
   )
 }
